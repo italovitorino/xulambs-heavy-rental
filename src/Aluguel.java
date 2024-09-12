a1813d7c5432c24705b497edd12a3a51aa740fd7
Ítalo Vitorino <italo.vitorinobs@hotmail.com>
2024-09-11 22:21:01 -0300

    [Parcial] iniciando refatoração da classe Equipamento

---
+++ src/Aluguel.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Aluguel {
+  
+  private LocalDate inicioAluguel;
+  private int duracaoAluguel;
+  private Equipamento equipamento;
+  private double valorDiario;
+
+  public Aluguel(Equipamento equipamento, LocalDate inicio, int duracao) {
+    this.inicioAluguel = inicio;
+    this.duracaoAluguel = duracaoAluguel;
+  }
+
+  public double valorAluguel() {
+    return valorDiario * duracaoAluguel;
+  }
+
+  public String relatorio() {
+
+  }
+
+  public boolean incluiData(LocalDate data) {
+    if (inicioAluguel == null)
+      return false;
+      
+    LocalDate fimAluguel = inicioAluguel.plusDays(duracaoAluguel);
+    return fimAluguel.isAfter(data);
+  }
+
+}
+
--- src/Equipamento.java
+++ src/Equipamento.java
@@ -13,11 +13,11 @@ public class Equipamento {
 
   private static int ultimoId = 0;
   private int id;
+  private Aluguel[] historico;
   private String descricao;
-  private LocalDate inicioAluguel;
-  private int duracaoAluguel;
   private int duracaoMaxima;
   private double valorDiaria;
+  private double descontoSemanal;
   private double totalArrecadado;
 
   /**
@@ -26,17 +26,20 @@ public class Equipamento {
    * - Descrição: "Equipamento" (se tiver menos de 5 caracteres)
    * - Valor da diária: 10 (se for menor ou igual a 0)
    * - Duração máxima: 7 (se for menor ou igual a 0)
+   * - Desconto: 0 (se não estiver entre 0 e 10)
    *
    * @param descricao     Descrição do equipamento, deve ter pelo menos 5
    *                      caracteres.
    * @param diaria        Valor da diária, deve ser maior que 0.
    * @param duracaoMaxima Duração máxima do aluguel, deve ser maior que 0.
+   * @param desconto      Desconto, em porcentagem, se determinado aluguel passar de 7 dias. Deve estar entre 0 e 10.
    */
-  public Equipamento(String descricao, double diaria, int duracaoMaxima) {
+  public Equipamento(String descricao, double diaria, int duracaoMaxima, double desconto) {
     this.id = ++ultimoId;
     this.descricao = (descricao.length() >= 5) ? descricao : "Equipamento";
-    this.valorDiaria = (diaria > 0) ? diaria : 10d;
-    this.duracaoMaxima = (duracaoMaxima > 0) ? duracaoMaxima : 7;
+    this.valorDiaria = diaria > 0 ? diaria : 10d;
+    this.duracaoMaxima = duracaoMaxima > 0 ? duracaoMaxima : 7;
+    this.descontoSemanal = desconto > 0 ? desconto <= 10 ? desconto / 100 : 0d : 0d;
     this.totalArrecadado = 0d;
   }
 
@@ -49,11 +52,15 @@ public class Equipamento {
    *         {@code false} se indisponível.
    */
   public boolean estaDisponivelEm(LocalDate data) {
-    if (inicioAluguel == null)
-      return true;
+    boolean disponivel = true;
 
-    LocalDate fimAluguel = inicioAluguel.plusDays(duracaoAluguel);
-    return fimAluguel.isBefore(data);
+    for (Aluguel aluguel : historico) {
+      if (aluguel.incluiData(data)) {
+        disponivel = false;
+      }
+    }
+    
+    return disponivel;
   }
 
   /**
@@ -69,24 +76,20 @@ public class Equipamento {
    *         o equipamento não estiver disponível ou se a duração do aluguel é
    *         inválida.
    */
-  public boolean alugar(LocalDate inicio, int duracaoAluguel) {
+  public Aluguel alugar(LocalDate inicio, int duracaoAluguel) {
     if (duracaoAluguel > 0 && duracaoAluguel <= this.duracaoMaxima && estaDisponivelEm(inicio)) {
-      this.inicioAluguel = inicio;
-      this.duracaoAluguel = duracaoAluguel;
+      
       this.totalArrecadado += valorDiaria * duracaoAluguel;
-      return true;
+      
     }
-
-    return false;
   }
 
-  /**
-   * Calcula o valor total do aluguel com base na duração e no valor da diária.
-   *
-   * @return O valor total do aluguel.
-   */
-  public double valorAluguel() {
-    return valorDiaria * duracaoAluguel;
+  public double totalArrecadado() {
+    return totalArrecadado;
+  }
+
+  public double valorDiario(int quantDias) {
+    //
   }
 
   /**
@@ -102,9 +105,12 @@ public class Equipamento {
 
     relatEquipamento.append(String.format("Descrição: %s\n", descricao));
     relatEquipamento.append(String.format("Valor da diária: %s\n", moeda.format(valorDiaria)));
-    relatEquipamento.append(inicioAluguel == null ? "Última locação: Sem registro!" : String.format("Última locação: %s - %s\n", inicioAluguel, inicioAluguel.plusDays(duracaoAluguel)));
     relatEquipamento.append(String.format("Total arrecadado: %s\n", moeda.format(totalArrecadado)));
 
     return relatEquipamento.toString();
+  }
+
+  public String relatorioAlugueis() {
+    //
   }
 }
