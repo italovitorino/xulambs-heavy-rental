eb4d9843c11f209e03daccea7d35ec6f8191394c
Ítalo Vitorino <italo.vitorinobs@hotmail.com>
2024-10-09 21:23:38 -0300

    Cadastrando novos equipamentos

--- XHRRentals/src/main/java/com/lpm/XHRRentals/Controllers/EquipamentoController.java
+++ XHRRentals/src/main/java/com/lpm/XHRRentals/Controllers/EquipamentoController.java
@@ -4,19 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lpm.XHRRentals.Models.Equipamento;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+
 @Controller
 public class EquipamentoController {
+
+  @PersistenceUnit
+  EntityManagerFactory factory;
   
   @PostMapping("/equipamentos")
-  public String cadastrarEquipamento(@RequestParam String descr,
+  public @ResponseBody Equipamento cadastrarEquipamento(@RequestParam String descricao,
                                      @RequestParam double diaria,
                                      @RequestParam int duracao,
                                      @RequestParam double desconto) {
-    Equipamento novoEquipamento = new Equipamento(descr, diaria, duracao, desconto);
+    EntityManager manager = factory.createEntityManager();
+
+    Equipamento novoEquipamento = new Equipamento(descricao, diaria, duracao, desconto);
+
+    manager.getTransaction().begin();
+    manager.persist(novoEquipamento);
+    manager.getTransaction().commit();
     
-    return entity;
+    return novoEquipamento;
   }
 }
