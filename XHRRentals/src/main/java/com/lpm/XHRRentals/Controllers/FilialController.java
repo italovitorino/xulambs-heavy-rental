f3626f52f26b5b496e7c3059eba325d1abc9a63e
Ítalo Vitorino <italo.vitorinobs@hotmail.com>
2024-10-16 22:01:16 -0300

    Criando model e controller[parcial] da Filial

---
+++ XHRRentals/src/main/java/com/lpm/XHRRentals/Controllers/FilialController.java
@@ -0,0 +1,33 @@
+package com.lpm.XHRRentals.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lpm.XHRRentals.Models.Filial;
+import com.lpm.XHRRentals.Models.Equipamento;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+
+@Controller
+public class FilialController {
+  
+  @PersistenceUnit
+  EntityManagerFactory factory;
+
+  @PostMapping("/filiais")
+  public @ResponseBody Filial cadastrarFilial(@RequestParam String nome) {
+    EntityManager manager = factory.createEntityManager();
+    Filial filial = new Filial(nome);
+
+    manager.getTransaction().begin();;
+    manager.persist(filial);
+    manager.getTransaction().commit();
+
+    return filial;
+  }
+
+}
---
+++ XHRRentals/src/main/java/com/lpm/XHRRentals/Models/Filial.java
@@ -0,0 +1,85 @@
+package com.lpm.XHRRentals.Models;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "filiais")
+public class Filial {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String nomeFilial;
+
+  @OneToMany
+  private List<Equipamento> equipamentos;
+
+  public Filial(){}
+
+  public Filial(String nome) {
+    if (nome.equals("")) {
+      nomeFilial = "Filial" + id;
+    }
+
+    nomeFilial = nome;
+    equipamentos = new LinkedList<>();
+  }
+
+  public int cadastrarEquipamento(Equipamento novo) {
+    equipamentos.add(novo);
+    return equipamentos.size();
+  }
+
+  public Equipamento buscarEquipamento(String descricao) {
+    Equipamento equipamento = null;
+
+    for (Equipamento e : equipamentos) {
+      if (e.getDescricao().equals(descricao)) {
+        equipamento = e;
+      }
+    }
+
+    return equipamento;
+  }
+
+  public Equipamento maiorArrecadacao() {
+    Equipamento equipamento = null;
+
+    for (Equipamento e : equipamentos) {
+      if (e.totalArrecadado() > equipamento.totalArrecadado()) {
+        equipamento = e;
+      }
+    }
+
+    return equipamento;
+  }
+
+  public Equipamento maiorArrecadacao(String equipamento1, String equipamento2) {
+    Equipamento e1 = buscarEquipamento(equipamento1);
+    Equipamento e2 = buscarEquipamento(equipamento2);
+    Equipamento maior = e1;
+
+    if (e2 != null && e2.totalArrecadado() > e1.totalArrecadado()) {
+      maior = e2;
+    }
+
+    return maior;
+  }
+
+  public String relatorioFilial() {
+    StringBuilder relatFilial = new StringBuilder();
+    relatFilial.append(String.format("ID: %ld", id));
+    relatFilial.append(String.format("Filial: %s", nomeFilial));
+
+    return relatFilial.toString();
+  }
+}
